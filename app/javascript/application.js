ec4b2cf3139b88ccdc6ae2cfa25ec5f73d41e9e7
SpinnyCow27 <matias.04@hotmail.es>
2024-08-13 03:32:52 -0400

    Configuracion para despliegue en render

---
+++ app/javascript/application.js
@@ -0,0 +1,18 @@
+// Entry point for the build script in your package.json
+// import "@hotwired/turbo-rails"
+import "./controllers"
+
+import { Turbo } from "@hotwired/turbo-rails";
+// app/javascript/application.js
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap CSS
+import 'bootstrap'; // Importa Bootstrap JS
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
+
+// Initialize Turbo
+Turbo.start();
+
+// Initialize Stimulus
+const application = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
